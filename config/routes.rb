e3d78e650094e2c812b160a0d372ecc95c1ab85d
occmium <occkart@yahoo.com>
2020-03-31 17:29:44 +0600

    Add controllers(main&rooms)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'rooms#index'
+  
+  resources :rooms, only: [:create, :index, :show], param: :token
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
