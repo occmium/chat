43075cd635ed83f76769cb68beb0087b32e287b0
occmium <occkart@yahoo.com>
2020-03-31 16:34:45 +0600

    Room model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_100523) do
+ActiveRecord::Schema.define(version: 2020_03_31_102603) do
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "token"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "nickname"
